8b268861f8f49809ca16db353783f6b668ef440d
Muhammad Arief <mgi.official1@gmail.com>
2023-06-09 02:54:24 +0700

    Remove template

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/template/html"
 )
 
 func main() {
-	engine := html.New("./views", ".html")
 	app := fiber.New(fiber.Config{
-		Views: engine,
+		AppName: "Perfect Panel",
 	})
 
 	err := app.Listen(":3000")
